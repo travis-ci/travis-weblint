51b9cc3895f21ba313bdf3a3aa88e03243bb4cc6
Kirill Lashuk <kirill.lashuk@gmail.com>
2012-07-10 22:09:46 +0300

    Use GitHub API v3.

--- spec/lib/validator_spec.rb
+++ spec/lib/validator_spec.rb
@@ -1,10 +1,11 @@
+require "base64"
 require "spec_helper"
 
 describe Travis::WebLint::Validator do
 
   describe ".validate_repo" do
     it "returns a valid result for a valid config" do
-      mock_http_requests_with_valid_config
+      mock_https_requests_with_valid_config
       mock_linter_with_valid_config
 
       result = Travis::WebLint::Validator.validate_repo("travis-ci/travis-ci")
@@ -12,7 +13,7 @@ describe Travis::WebLint::Validator do
     end
 
     it "returns an invalid result for an invalid config" do
-      mock_http_requests_with_invalid_config
+      mock_https_requests_with_invalid_config
       mock_linter_with_invalid_config
 
       result = Travis::WebLint::Validator.validate_repo("travis-ci/travis-ci")
@@ -36,27 +37,36 @@ describe Travis::WebLint::Validator do
     end
   end
 
-  def mock_http_requests_with_valid_config
-    http_sequence = mock_http_sha_request
-
-    blob_response = mock(:body => '{ "blob": { "data": "language: ruby\nrvm:\n  - 1.9.3" } }')
-    Net::HTTP.expects(:get_response).returns(blob_response).in_sequence(http_sequence)
+  def mock_https_requests_with_valid_config
+    mock_https_get(blob_content("language: ruby\nrvm:\n  - 1.9.3"), mock_https_sha_request)
   end
 
-  def mock_http_requests_with_invalid_config
-    http_sequence = mock_http_sha_request
-
-    blob_response = mock(:body => '{ "blob": { "data": "rvm:\n  - 1.9.3" } }')
-    Net::HTTP.expects(:get_response).returns(blob_response).in_sequence(http_sequence)
+  def mock_https_requests_with_invalid_config
+    mock_https_get(blob_content("rvm:\n  - 1.9.3"), mock_https_sha_request)
   end
 
-  def mock_http_sha_request
-    http_sequence = sequence("http_requests")
+  def mock_https_sha_request
+    mock_https_get('[{ "sha": "b7fbb8b3479f118c1682271014503b1255a8ba02" }]')
+  end
 
-    sha_response = mock(:body => '{ "commits": [{ "tree": "b7fbb8b3479f118c1682271014503b1255a8ba02" }] }')
-    Net::HTTP.expects(:get_response).returns(sha_response).in_sequence(http_sequence)
+  def blob_content(string)
+    %Q({ "content": "#{Base64.encode64(string).gsub(/\n/, '\\n')}" })
+  end
 
-    http_sequence
+  def mock_https_get(response_body, sequence = sequence("https_request"))
+    https = Object.new
+
+    Net::HTTP.expects(:new).with('api.github.com', 443).returns(https).in_sequence(sequence)
+    https.expects(:use_ssl=).with(true).in_sequence(sequence)
+    https.expects(:get).returns(response_with_body(response_body)).in_sequence(sequence)
+
+    sequence
+  end
+
+  def response_with_body(body)
+    response = Net::HTTPOK.new("1.1", "200", "OK")
+    response.stubs(:body).returns(body)
+    response
   end
 
   def mock_linter_with_valid_config
